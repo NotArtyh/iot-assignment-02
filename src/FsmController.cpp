55246be659950425617c9bfa648660a1ca831e30
Giuseppe Cattolico <giuseppe.cattolico@studio.unibo.it>
2026-02-23 22:09:10 +0100

    Implement FSM with LED and Button control, including Timer functionality

---
+++ src/FsmController.cpp
@@ -0,0 +1,60 @@
+#include "Arduino.h"
+#include "HangarDevice/Actuator/Led/Led.h"
+#include "Timer.h"
+#include "HangarDevice/Actuator/Button/ButtonImpl.h"
+
+#define LED_PIN 13
+#define BUTTON_PIN 2
+
+Light *led;
+Button *button;
+Timer timer;
+
+enum
+{
+  ON,
+  OFF
+} state;
+
+//QUI SI INIZIALIZZANO SOLO I VARI OGGETTI E LO STATO INIZIALE
+void setup()
+{
+  Serial.begin(9600);
+  led = new Led(LED_PIN);
+  button = new ButtonImpl(BUTTON_PIN);
+  state = OFF;
+  timer.setupPeriod(50);
+}
+
+void step()
+{
+  bool isPressed;
+  switch (state)
+  {
+  case OFF:
+    isPressed = button->isPressed();
+    if (isPressed)
+    {
+      led->switchOn();
+      Serial.println("ON");
+      state = ON;
+    }
+    break;
+  case ON:
+    isPressed = button->isPressed();
+    if (!isPressed)
+    {
+      led->switchOff();
+      Serial.println("OFF");
+      state = OFF;
+    }
+    break;
+  }
+}
+
+//QUESTO DEVE RIMANERE COSI
+void loop()
+{
+  timer.waitForNextTick();
+  step();
+};
